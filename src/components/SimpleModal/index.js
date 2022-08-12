d817e71ce3b555224236eb919ffa38bc75c95c9f
linyuan1105 <673609957@qq.com>
2022-08-12 17:24:56 +0800

    feature: react 个人电脑win11弹框组件拖拽

--- src/components/SimpleModal/index.js
+++ src/components/SimpleModal/index.js
@@ -6,41 +6,64 @@ export function SimpleDialogContainer(props) {
   const active = props.active
   const appName = props.appName
   const close = props.close || (() => {})
-  const [width] = useState('50%')
-  const [height] = useState('50%')
+  const [curWidth] = useState('30%')
+  const [curHeight] = useState('30%')
+
+  // 顶部拉伸
+
+  // 移动弹框
+  const [sTop] = useState('50%')
+  const [sLeft] = useState('50%')
+  const onMoverMouseDown = (e) => {
+    console.log(e)
+  }
   return (
     <>
       <div
         className={
           `${styles.simpleModalContent} ` + (active ? `${styles.simpleModalActive}` : '')
         }
+        onMouseDown={onMoverMouseDown}
         style={{
-          width,
-          height,
+          width: curWidth,
+          height: curHeight,
+          top: sTop,
+          left: sLeft,
         }}
       >
         <div className={styles.simpleModalHeader}>
-          <div className={styles.simpleModalHeaderTitle}>{ appName }</div>
+          <div className={styles.simpleModalHeaderTitle}>
+            <div
+              className={styles.simpleModalContentMover}
+              data-value='moverMouseDown'
+            ></div>
+            { appName }
+          </div>
           <div className={ styles.simpleModalHeaderIcons  } onClick={close}>
             X
           </div>
         </div>
         <div className={styles.simpleModalBody}>
-          {props.children || '暂无内容'}
+          {props.children || null}
         </div>
         <div className={styles.simpleModalFooter}></div>
 
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div
+          className={styles.simpleModalContentTop}
+        ></div>
+        <div className={styles.simpleModalContentRight}></div>
+        <div className={styles.simpleModalContentBottom}></div>
+        <div className={styles.simpleModalContentLeft}></div>
       </div>
     </>
   )
 }
 SimpleDialogContainer.propTypes = {
   active: PropTypes.bool,
-  children: PropTypes.element,
+  children: PropTypes.oneOfType([
+    PropTypes.element,
+    PropTypes.string
+  ]),
   close: PropTypes.func,
   appName: PropTypes.string,
 }
